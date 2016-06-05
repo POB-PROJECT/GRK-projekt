665f08e945df3bbfd54f10911865b8580486aa77
deniso95 <denis.olek@gmail.com>
2016-06-05 04:06:02 +0200

    light

--- rybahere/main.cpp
+++ rybahere/main.cpp
@@ -21,10 +21,13 @@ int main(int argc, char** argv)
 		Vertex(glm::vec3(0,0.5,0), glm::vec2(0.5,1.0)),
 		Vertex(glm::vec3(0.5,-0.5,0), glm::vec2(1.0,0.0))
 	};
+
+	unsigned int indices[] = { 0, 1, 2 };
 	
-	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices)/sizeof(indices[0]));
+	Mesh mesh2("./res/monkey3.obj");
 	Transform transform;
-	Camera camera(glm::vec3(0, 0, -3), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
+	Camera camera(glm::vec3(0, 0, -5), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
 	float counter = 0.0f;
 	while (!display.IsClosed()){
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
@@ -43,6 +46,7 @@ int main(int argc, char** argv)
 		texture.Bind(0);
 		shader.Update(transform, camera);
 		mesh.Draw();
+		mesh2.Draw();
 
 		display.Update();
 		counter += 0.01f;
