caae5f113915609090639ba4e0732b829994ef9f
deniso95 <denis.olek@gmail.com>
2016-06-06 05:51:53 +0200

    sand, rock, bubbles

--- rybahere/main.cpp
+++ rybahere/main.cpp
@@ -15,23 +15,31 @@ int main(int argc, char** argv)
 	Display display(WIDTH, HEIGHT, "Ryba Here!");
 	Shader shader("./res/basicShader");
 	Texture texture("./res/bricks.jpg");
-	
-	Vertex vertices[] = {
-		Vertex(glm::vec3(-0.5,-0.5,0), glm::vec2(0.0,0.0)),
-		Vertex(glm::vec3(0,0.5,0), glm::vec2(0.5,1.0)),
-		Vertex(glm::vec3(0.5,-0.5,0), glm::vec2(1.0,0.0))
-	};
+	Texture bottomTexture("./res/bottomTexture.jpg");
+	Texture rockTexture("./res/rockTexture.jpg");
+	Texture bubbleTexture("./res/bubbleTexture.jpg");
 
-	unsigned int indices[] = { 0, 1, 2 };
-	
-	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices)/sizeof(indices[0]));
+
 	Mesh mesh2("./res/monkey3.obj");
+	Mesh mesh3("./res/Rock.obj");
+	Mesh bottomSandMesh("./res/untitled.obj");
+	Mesh bubbleMesh("./res/bubble.obj");
+
 	Transform transform;
+	Transform transform1;
+	Transform bottomTransform;
+	Transform bubbleTransform;
 	Camera camera(glm::vec3(0, 0, -10), 70.0f, (float)WIDTH / (float)HEIGHT, 0.01f, 1000.0f);
 	float counter = 0.0f;
+	float bubbleCounter = -2.0f;
 	float rotatePosition = 3.0f;
 	float rangePosition = 4.0f;
 	transform.GetRot().y = 1.86f;
+	transform1.GetPos().x = -5.7f;
+	transform1.GetPos().y = -1.1f;
+	bottomTransform.GetPos().y = -5.0f;
+	bottomTransform.GetScale().x = 1.7f;
+	bubbleTransform.GetPos().x = 5.0f;
 	while (!display.IsClosed()){
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
 
@@ -53,9 +61,14 @@ int main(int argc, char** argv)
 			transform.GetPos().x = sinCounter * rangePosition;
 		}
 		transform.GetRot().z = sinCounter/4;
-		std::cout << "POS X: " << transform.GetPos().x << std::endl;
+		std::cout << "POS Y: " << bubbleTransform.GetPos().y << std::endl;
 		transform.GetPos().z = cosCounter;
 
+		if (bubbleTransform.GetPos().y > 10.0f)
+		{
+			bubbleCounter = -2.0f;
+		}
+		bubbleTransform.GetPos().y = bubbleCounter *4;
 
 		//transform.GetRot().x = counter * 3;
 		//transform.GetRot().y = counter * 3;
@@ -65,11 +78,20 @@ int main(int argc, char** argv)
 		shader.Bind();
 		texture.Bind(0);
 		shader.Update(transform, camera);
-		mesh.Draw();
 		mesh2.Draw();
+		shader.Update(transform1, camera);
+		rockTexture.Bind(0);
+		mesh3.Draw();
+		shader.Update(bottomTransform, camera);
+		bottomTexture.Bind(0);
+		bottomSandMesh.Draw();
+		shader.Update(bubbleTransform, camera);
+		bubbleTexture.Bind(0);
+		bubbleMesh.Draw();
 
 		display.Update();
 		counter += 0.01f;
+		bubbleCounter += 0.01f;
 	}
 	return 0;
 }
